268f3b469ada352a5e982d8779fd598daa01f90b
hYdos <haydenv06@gmail.com>
2020-03-10 16:51:49 +1000

    glfw

---
+++ Dune/DuneExample.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <GLFW/glfw3.h>
+
+
+int main()
+{
+    std::cout << "Hello Dune!\n";
+    if (!glfwInit()) { // initialise glfw
+        fprintf(stderr, "Failed to initialise glfw\n");
+        return -1;
+    }
+    glfwWindowHint(GLFW_SAMPLES, 4); // 4x antialiasing
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3); // We want OpenGL 3.3
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // To make MacOS happy; should not be needed
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); // We don't want the old OpenGL 
+
+    // Open a window and create its OpenGL context
+    GLFWwindow* window; // (In the accompanying source code, this variable is global for simplicity)
+    window = glfwCreateWindow(1200, 800, "Dune example", NULL, NULL);
+    if (window == NULL) {
+        fprintf(stderr, "tiny potato\n");
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window); // Initialize GLEW
+    while (!glfwWindowShouldClose(window)) {
+
+    }
+}
